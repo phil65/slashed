d8011d0e227e2cb39f5d8523ae63508fa450b4a2
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-11-05 23:02:51 +0100

    chore: kwargs for command fns

--- src/slashed/builtin/__init__.py
+++ src/slashed/builtin/__init__.py
@@ -17,18 +17,41 @@ if TYPE_CHECKING:
     from slashed.base import BaseCommand
 
 
-def get_builtin_commands() -> list[BaseCommand]:
+def get_builtin_commands(
+    *,
+    enable_help: bool = True,
+    enable_exit: bool = True,
+) -> list[BaseCommand]:
     """Get list of built-in commands."""
-    return [HelpCommand(), ExitCommand()]
+    commands = []
+    if enable_help:
+        commands.append(HelpCommand())
+    if enable_exit:
+        commands.append(ExitCommand())
+    return commands
 
 
-def get_system_commands() -> list[BaseCommand]:
+def get_system_commands(
+    *,
+    enable_exec: bool = True,
+    enable_run: bool = True,
+    enable_processes: bool = True,
+    enable_sysinfo: bool = True,
+    enable_kill: bool = True,
+    enable_env: bool = True,
+) -> list[BaseCommand]:
     """Get system execution commands."""
-    return [
-        ExecCommand(),
-        RunCommand(),
-        ProcessesCommand(),
-        SystemInfoCommand(),
-        KillCommand(),
-        EnvCommand(),
-    ]
+    commands = []
+    if enable_exec:
+        commands.append(ExecCommand())
+    if enable_run:
+        commands.append(RunCommand())
+    if enable_processes:
+        commands.append(ProcessesCommand())
+    if enable_sysinfo:
+        commands.append(SystemInfoCommand())
+    if enable_kill:
+        commands.append(KillCommand())
+    if enable_env:
+        commands.append(EnvCommand())
+    return commands
